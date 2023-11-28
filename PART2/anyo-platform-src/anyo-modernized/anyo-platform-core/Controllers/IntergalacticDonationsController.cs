9e013a101d5b14f79637915863f8a06e420490ab
Mo Mohoboob <momohob@amazon.com>
2023-11-28 10:01:37 -0800

    update microservice create method

--- PART2/anyo-platform-src/anyo-modernized/anyo-platform-core/Controllers/IntergalacticDonationsController.cs
+++ PART2/anyo-platform-src/anyo-modernized/anyo-platform-core/Controllers/IntergalacticDonationsController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mime;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
@@ -10,42 +11,35 @@ using anyo_platform_core.Data;
 using System.Security.Claims;
 using anyo_platform_core.Data.ViewModels;
 using System.Security.Policy;
+using System.Text;
 using Newtonsoft.Json;
 using Microsoft.AspNetCore.Authorization;
-
 namespace anyo_platform_core.Controllers
 {
     [Authorize]
     public class IntergalacticDonationsController : Controller
     {
         private readonly ApplicationDbContext _context;
-
         public IntergalacticDonationsController(ApplicationDbContext context)
         {
             _context = context;
         }
-
         public async Task<IActionResult> MicroServiceIndex()
         {
             string url = "https://localhost:7704/Donations/Get";
-
             using (HttpClient client = new HttpClient())
             {
                 var json = await client.GetStringAsync(url);
                 var apiModel = JsonConvert.DeserializeObject<DonationsViewModel>(json);
-
                 if (apiModel == null)
                     return BadRequest();
-
                 return View(apiModel);
             }
         }
-
         public async Task<IActionResult> Index()
         {
             var applicationDbContext = _context.IntergalacticDonation;
             var indexViewModel = new DonationsViewModel();
-
             foreach (var donation in applicationDbContext.ToList())
             {
                 var model = new DonationViewModel()
@@ -57,13 +51,10 @@ namespace anyo_platform_core.Controllers
                     Package = _context.IntergalacticPackages.FirstOrDefault(m => m.Id == donation.PackageId),
                     DonorName = donation.DonorName
                 };
-
                 indexViewModel.Donations.Add(model);
             }
-
             return View(indexViewModel);
         }
-
         public IActionResult Create(int? id)
         {
             ViewData["DonorName"] = User.Identity.Name;
@@ -71,7 +62,27 @@ namespace anyo_platform_core.Controllers
             ViewData["PackageId"] = new SelectList(_context.IntergalacticPackages, "Id", "Name");
             return View();
         }
-
+        [HttpPost]
+        public async Task<IActionResult> MicroserviceCreate([Bind("Message,Quantity,MissionId,DonorName,PackageId")] IntergalacticDonation intergalacticDonation)
+        {
+            if (ModelState.IsValid)
+            {
+                var url = "https://localhost:7704/Donations/Create";
+                using (var client = new HttpClient())
+                {
+                    var json = JsonConvert.SerializeObject(intergalacticDonation);
+                    var request = new HttpRequestMessage
+                    {
+                        Method = HttpMethod.Post,
+                        RequestUri = new Uri(url),
+                        Content = new StringContent(json, Encoding.UTF8, MediaTypeNames.Application.Json),
+                    };
+                    var response = await client.SendAsync(request);
+                    return Ok();
+                }
+            }
+            return BadRequest();
+        }
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("Message,Quantity,MissionId,DonorName,PackageId")] IntergalacticDonation intergalacticDonation)
@@ -79,24 +90,19 @@ namespace anyo_platform_core.Controllers
             if (ModelState.IsValid)
             {
                 var mission = _context.IntergalacticMissions.Find(intergalacticDonation.MissionId);
-
                 if (mission != null)
                 {
                     mission.Current += intergalacticDonation.Quantity;
                 }
-
                 _context.Add(intergalacticDonation);
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
-
             ViewData["DonorName"] = intergalacticDonation.DonorName;
             ViewData["MissionId"] = intergalacticDonation.MissionId;
             ViewData["PackageId"] = new SelectList(_context.IntergalacticPackages, "Id", "Name", intergalacticDonation.PackageId);
             return View(intergalacticDonation);
         }
-
-
         private bool IntergalacticDonationExists(int id)
         {
           return (_context.IntergalacticDonation?.Any(e => e.Id == id)).GetValueOrDefault();
